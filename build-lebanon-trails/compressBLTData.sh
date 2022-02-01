7eb0651014a16ca467589fb55963037bc8139e75
RobLabs.com <roblabs@users.noreply.github.com>
2022-02-01 10:39:13 -0800

    Add compress script.  Relative Path

---
+++ build-lebanon-trails/compressBLTData.sh
@@ -0,0 +1,9 @@
+zip -r BLT-QGIS-source-trail-data.zip \
+  Northside-Community-Trail \
+  Trails \
+  West-River-Trail \
+  blt.qgs \
+  buildLebanonTrails.geojson \
+  cropped-blt-logo-rework-e01-final.png \
+  map.osm \
+  OMT-EST-connector.geojson
